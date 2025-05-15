b7a79b8d6b03ee899937160b12c1eaaa810cd119
JuanDAC <juanavila12.0308@gmail.com>
2025-05-15 16:15:49 +0000

    Fix webdriver

--- src/scrapper.py
+++ src/scrapper.py
@@ -1,6 +1,7 @@
 # farmatodo_scrapper/src/scrapper.py
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
